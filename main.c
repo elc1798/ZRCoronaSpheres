7975dc299cf755bab440f58898ee41002d002120
elc1798 <subliminalmau5@gmail.com>
2014-09-14 22:21:50 -0400

    Added moveToZone functions

--- main.c
+++ main.c
@@ -11,14 +11,22 @@ void init(){
 	for (i = 0; i < 3; i++){
 		asteroid[i] = 0.0f;
 	}
-	safeZone[0] = 0.6f;
+	safeZone[0] = 0.4f;
 	safeZone[1] = 0.0f;
 	safeZone[2] = 0.0f;
 }
 
 void moveToSafeZone(){
 	//Tells the Robot to move to the safe zone
-	api.setPositionTarget(safeZone);
+	if (position[1] < 0) {
+		api.setPositionTarget({0.4 , position[1] , position[2]});
+	}
+	else {
+		api.setPositionTarget({-0.4 , position[1] , position[2]});
+	}
+	if (position[0] > 0.2) {
+		api.setPositionTarget(safeZone);
+	}
 }
 
 float getDist(float start[3] , float end[3]){
@@ -34,16 +42,28 @@ void moveToOuter(){
 		api.setPositionTarget(asteroid);
 	}
 	if (getDist(position , asteroid) < 0. 42) {
-		targetRate[0] = PI;
-		targetRate[1] = 0.0f;
-		targetRate[2] = 0.0f;
-		api.setAttRateTarget(targetRate);
-		
+		//Robot must move AWAY from asteroid now
+		if (position[0] < 0) {
+			api.setPositionTarget({-0.6 , 0 , 0});
+		}
+		else {
+			api.setPositionTarget({0.6 , 0 , 0});
+		}
 	}
 }
 
 void moveToInner(){
-
+	if (getDist(position , asteroid) > 0.33) {
+		api.setPositionTarget(asteroid);
+	}
+	if (getDist(position , asteroid) < 0.33) {
+		if (position[0] < 0) {
+			api.setPositionTarget({-0.6 , 0 , 0});
+		}
+		else {
+			api.setPositionTarget({0.6 , 0 , 0});
+		}
+	}
 }
 
 void checkUpload(){
